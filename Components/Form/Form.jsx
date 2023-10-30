34b2bdefa846ffc559a509f3b6bb713dd730202f
hakeema51.alsoudi@gmail.com <hakeema51.alsoudi@gmail.com>
2023-10-30 20:10:06 +0300

    Done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,11 +1,38 @@
-export default function Form() {
+/** @format */
+
+import React, { useState } from "react";
+
+export default function Form({ setUsername }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUsername(inputValue);
+    setInputValue("");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>Search user Github</h3>
       <div className="mb-3">
         <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="User Name"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
       </div>
+      <button
+        type="submit"
+        className="btn btn-primary">
+        Search
+      </button>
     </form>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,43 @@
-import Form from '../Components/Form/Form'
-import UserCard from '../Components/UserCard/UserCard'
-import { Inter } from 'next/font/google'
+/** @format */
+import React, { useState, useEffect } from "react";
+import Form from "../Components/Form/Form";
+import UserCard from "../Components/UserCard/UserCard";
+import { Inter } from "next/font/google";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    // Function to fetch user data from GitHub API
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setUserData(data);
+        } else {
+          setUserData(null);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setUserData(null);
+      }
+    };
+
+    if (username) {
+      fetchUserData();
+    }
+  }, [username]);
+
   return (
     <div className="card">
-      <Form />
-      <UserCard />
+      <Form setUsername={setUsername} />
+      <UserCard userData={userData} />
     </div>
   );
 }
